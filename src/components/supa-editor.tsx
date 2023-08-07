e094fefe6b41123d6099c710c1d97f34a4fc0eb8
Xavier Alfaro <nxb.developer@gmail.com>
2023-08-06 23:09:48 -0500

    add line editing constraints

--- src/components/supa-editor.tsx
+++ src/components/supa-editor.tsx
@@ -1,4 +1,5 @@
-import Editor from '@monaco-editor/react'
+import Editor, { type Monaco } from '@monaco-editor/react'
+import { constrainedEditor } from 'constrained-editor-plugin'
 
 type SupaEditorProps = {
   defaultCode: string
@@ -6,6 +7,30 @@ type SupaEditorProps = {
 }
 
 export function SupaEditor({ defaultCode, onChangeCode }: SupaEditorProps) {
+  const handleEditorDidMount = (editor: any, monaco: Monaco) => {
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      ...monaco.languages.typescript.typescriptDefaults.getCompilerOptions(),
+      module: monaco.languages.typescript.ModuleKind.ESNext,
+      target: monaco.languages.typescript.ScriptTarget.ESNext,
+      moduleResolution: monaco.languages.typescript.ModuleResolutionKind.NodeJs,
+      isolatedModules: true,
+      allowJs: true,
+      strict: true,
+      skipLibCheck: true
+    })
+
+    const constrainedInstance = constrainedEditor(monaco)
+    const model = editor.getModel()
+    constrainedInstance.initializeIn(editor)
+    constrainedInstance.addRestrictionsTo(model, [
+      {
+        range: [2, 1, 2, 1], // Range of Function definition
+        allowMultiline: true,
+        label: 'funcDefinition'
+      }
+    ])
+  }
+
   return (
     <Editor
       options={{
@@ -25,6 +50,7 @@ export function SupaEditor({ defaultCode, onChangeCode }: SupaEditorProps) {
       defaultValue={defaultCode}
       theme='vs-dark'
       loading={<span className='text-white'>Loading editor...</span>}
+      onMount={handleEditorDidMount}
     />
   )
 }
