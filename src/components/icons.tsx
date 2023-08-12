c29bc62b4b8faa3ea02471068bfc19a61e5d2ea3
Xavier Alfaro <nxb.developer@gmail.com>
2023-08-11 23:11:05 -0500

    add new icon

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -23,3 +23,11 @@ export function GearIc(props: SVGProps<SVGSVGElement>) {
     </svg>
   )
 }
+
+export function CheckIc(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' fill='currentColor' viewBox='0 0 256 256' {...props}>
+      <path d='M229.66,77.66l-128,128a8,8,0,0,1-11.32,0l-56-56a8,8,0,0,1,11.32-11.32L96,188.69,218.34,66.34a8,8,0,0,1,11.32,11.32Z'></path>
+    </svg>
+  )
+}
