d3f3253ac2ffa10d8989953fe0e55126faa36a94
Xavier Alfaro <nxb.developer@gmail.com>
2023-08-11 23:07:49 -0500

    fix attribute

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -15,7 +15,7 @@ export function LoadingSpin(props: SVGProps<SVGSVGElement>) {
         cy='12'
         r='10'
         stroke='currentColor'
-        stroke-width='4'
+        strokeWidth='4'
       ></circle>
       <path
         className='opacity-75'
